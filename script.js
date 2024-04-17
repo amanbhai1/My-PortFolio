e0b6e2b5ffdd80c6985027c0ffbd5e2cf4c591de
amanbhai1 <49727102+amanbhai1@users.noreply.github.com>
2024-04-17 20:57:49 +0530

    This Portfolio Was Made by me.

---
+++ script.js
@@ -0,0 +1,32 @@
+var typed= new Typed(".text",{
+    strings:["Frontend Developer", "Web Developer", "App Developer","Database Designer"],
+    typeSpeed:100,
+    backSpeed:100,
+    backDelay:1000,
+    loop:true
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
